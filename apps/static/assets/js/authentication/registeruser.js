4bab1ba5ee4082e98b9ab62839f83e19a85f7007
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2024-11-09 13:18:25 +0530

    register codejs

--- apps/static/assets/js/authentication/registeruser.js
+++ apps/static/assets/js/authentication/registeruser.js
@@ -1,23 +1,55 @@
+// function register_user() {
+//     const phoneNumber = $('#phonenumber').val();
+//     const email = $('#useremail').val();
+//     const pan = $('#pan').val();
+//     const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+
+//     $.ajax({
+//         url: '/user_register/',
+//         type: 'POST',
+//         data: {
+//             'phone_number': phoneNumber,
+//             'email': email,
+//             'pan': pan,
+//             'csrfmiddlewaretoken': csrfToken
+//         },
+//         success: function (response) {
+//             window.location = response['path']
+//         },
+//         error: function (response) {
+//             show_error(response.responseJSON['error'])
+//         }
+//     });
+// }
+
 function register_user() {
+    const username = $('#validationTooltipUsername').val();
     const phoneNumber = $('#phonenumber').val();
     const email = $('#useremail').val();
-    const pan = $('#pan').val();
+    const city = $('#validationCustom03').val();
+    const state = $('#validationCustom04').val();
+    const zip = $('#validationCustom05').val();
+    const password = $('#example-password-input').val();
     const csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
 
     $.ajax({
         url: '/user_register/',
         type: 'POST',
         data: {
+            'username': username,
             'phone_number': phoneNumber,
             'email': email,
-            'pan': pan,
+            'city': city,
+            'state': state,
+            'zip': zip,
+            'password': password,
             'csrfmiddlewaretoken': csrfToken
         },
         success: function (response) {
-            window.location = response['path']
+            window.location = response['/'];
         },
         error: function (response) {
-            show_error(response.responseJSON['error'])
+            show_error(response.responseJSON['error']);
         }
     });
 }
